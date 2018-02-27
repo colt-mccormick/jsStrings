0f7cca74169565ac8a712e662839dc0cae0e9068
Colt McCormick <coltmccormick@hotmail.com>
2018-02-26 20:42:16 -0600

    completed challenge #3

--- strings.js
+++ strings.js
@@ -10,3 +10,12 @@ console.log("numOfSandwiches", numOfSandwiches)//should print 8
 var name ="Colt"
 var string = ("Hello, " + name + " how are you doing today?");
 console.log("string", string); //should print out "Hello, Colt how are you doing today?"
+
+//challenge 3
+
+var DNA ="GCAT"
+
+//var RNA = DNA[0] + DNA[1] + DNA[2] + "U"
+//var RNA = DNA.slice(0,3) + "U"
+var RNA = DNA.replace("T", "U")
+console.log("RNA:", RNA)//should print out GCAU
