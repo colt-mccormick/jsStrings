4de728ae92be8ac57ecec7480c6c6e5aacc17117
Colt McCormick <coltmccormick@hotmail.com>
2018-02-26 20:31:02 -0600

    completed exercise #2

--- strings.js
+++ strings.js
@@ -2,6 +2,11 @@ console.log("hello world!!!!");
 
 //challenge 1
 
-
 var numOfSandwiches= 4*2; 
 console.log("numOfSandwiches", numOfSandwiches)//should print 8
+
+//challenge 2
+
+var name ="Colt"
+var string = ("Hello, " + name + " how are you doing today?");
+console.log("string", string); //should print out "Hello, Colt how are you doing today?"
